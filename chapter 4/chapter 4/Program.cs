f4cc4075a9b0715edd51e0662d2a8ee18f279948
Btuturice <btuturice@outlook.com>
2016-02-12 11:31:01 -0500

    part1

---
+++ chapter 4/chapter 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             int input;
+             for (int i = 1; i > 0; i++)
+            {
+                 
+                 Console.WriteLine("Enter a number");
+                 input = Convert.ToInt32(Console.ReadLine());
+                 int sum = 0;
+
+                 while (input == 999)
+                 {
+                     break;
+                 }
+            }
+        }
+    }
+}
