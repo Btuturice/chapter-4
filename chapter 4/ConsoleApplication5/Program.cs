82a9b1e1f359480e2af49eb1ea65f341b6c217be
Btuturice <btuturice@outlook.com>
2016-02-24 11:28:26 -0500

    trf

---
+++ chapter 4/ConsoleApplication5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sqaure;
+            for (int x = 1; x <= 20; x++)
+            {
+                Console.WriteLine("Integer: " + x);
+                square = x * x;
+                Console.WriteLine("Sqaure: " + sqaure);
+            }
+        }
+    }
+}
